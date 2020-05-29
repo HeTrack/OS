9a84ed13adf416d13b523d8ddecfdbe24b54eafc
HeTrack <iliya.utd2014@gmail.com>
2020-05-29 11:56:07 +0400

    TablePages

---
+++ laba2_OS/src/laba2_OS/TablePages.java
@@ -0,0 +1,28 @@
+package laba2_OS;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TablePages {
+	private ArrayList<Page> pages = new ArrayList<Page>();
+
+    public Page removePageWithAddress(int address) {
+        for (Page page : pages) {
+            if (page.isHasAddress(address)) {
+                 return pages.remove(pages.indexOf(page));
+            }
+        }
+
+        return null;
+    }
+
+    public void setPage(Page page) {
+        pages.add(page);
+    }
+
+    public Page getPage() {
+        Random random = new Random();
+        return pages.remove(random.nextInt(pages.size()));
+    }
+}
+
